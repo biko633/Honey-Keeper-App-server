6445ce1b744fd91a71d18419055ddafe75b488a2
biko <ahmadyaslam925@gmail.com>
2024-03-26 15:30:10 +0300

    fix email 2

--- src/middlewares/sendEmail.js
+++ src/middlewares/sendEmail.js
@@ -29,7 +29,7 @@ export const sendMail = (email, email_token) => {
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console, log(error);
+      console.log(error);
     } else {
       console.log("Email sent: " + info.response);
     }
